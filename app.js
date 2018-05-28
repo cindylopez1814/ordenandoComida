4cc556f6e92d03836eeb7adc879251f288f45e8b
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-05-28 18:25:09 -0400

    ejercicio resuelto

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 const btn = document.getElementById("button") 
 
-    btn.addEventListener("click", function () {
+  btn.addEventListener("click", function () {
 
-        let orden = prompt("¿qué deseas pedir hoy?")
-        switch (orden){
-            case "ceviche":
-            alert("lo sinto se a acabado por hoy")
-            prompt("ordenar algo mas")
-            case "tacos al pastor":
-            alert("lo sinto se a acabado por hoy")
-            prompt("ordenar algo mas")
-            case "empanada":
-            alert("lo sinto se a acabado por hoy")
-            prompt("ordenar algo mas")
-            break;
-            case "hamburguesa":
-            alert("perfecto en unos minutos tu pedido esta listo")
-            break;
-            default:
-            alert("Lo siento,No esta en el menu de hoy")
-            break;
-        }
+      let orden = prompt("¿qué deseas pedir hoy?")
+      switch (orden){
+          case "ceviche":
+          prompt("lo sinto se a acabado por hoy ¿deseas ordenar algo mas?")
+          case "tacos al pastor":
+          prompt("lo sinto se a acabado por hoy ¿deseas ordenar algo mas?")
+          case "empanada":
+          prompt("lo sinto se a acabado por hoy ¿deseas ordenar algo mas?")
+          break;
+          case "hamburguesa":
+          alert("perfecto en unos minutos tu pedido estara listo en unos minutos")
+          break;
+          default:
+          alert("Lo siento,No esta en el menu de hoy")
+        };
       
       });
 
